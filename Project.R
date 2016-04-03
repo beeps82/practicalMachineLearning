8f28fce8467a587c5af385e9648bca5b34e6395b
o.O <o.O>
2016-04-03 03:37:04 -0400

    assignment

--- Project.R
+++ Project.R
@@ -1,19 +1,23 @@
-data<- read.csv(file="C:/Users/Acer User/Documents/R/pml-training.csv");
 library(ggplot2)
 library(lattice)
 library(AppliedPredictiveModeling)
 library(caret)
 library(rpart)
 library(corrplot)
+library(randomForest)
 
 set.seed(1250)
 
 #foldTrain<- createFolds(y = data$classe, k= 10,list = TRUE,returnTrain = TRUE)
 #foldTest<- createFolds(y = data$classe, k= 10,list = TRUE, returnTrain = FALSE)
 
+
+data<- read.csv(file="C:/Users/Acer User/Documents/coursera/PracticalMachineLearning/pml-training.csv");
+
+
 # Pre-processing
 
-tmp<- training[,!nsV$nzv]
+#tmp<- training[,!nsV$nzv]
 
 data$classe = factor(data$classe)
 
@@ -30,33 +34,43 @@ preData<- data[,(!naCols & !nsV$nzv)]
 
 corrplot(cor(preData[,-53]), method = "circle",type = 'upper')
 
-pcaData<- preProcess(preData[,-53],method ='pca', thresh =0.8, na.rm = TRUE)
 
+#descrCor<-  cor(preData[,-53])
+#highCorr<- sum(abs(descrCor[upper.tri(descrCor)]) > .95)
+#highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
 
-
-descrCor<-  cor(preData[,-53])
-highCorr<- sum(abs(descrCor[upper.tri(descrCor)]) > .95)
-highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
-
-
-
-
-
-highCorr<- sum(abs(prds[upper.tri(prds)]) > .999)
-corrplot(preData[,-53], method = "number")
 
 inTrain<- createDataPartition(y=preData$classe, p=0.75,list = FALSE);
 training<- preData[inTrain,];
-testing<- preDdata[-inTrain,];
+validation<- preData[-inTrain,];
+
+pcaData<- preProcess(training[,-53],method ='pca', thresh =0.8, na.rm = TRUE)
+trainData<- predict(pcaData,training[,-53])
+
+ # Use the trainSet dataframe as the training data
+# Use cross-validation
+trainModel<- train(training$classe ~ .,data = trainData, method = "rf",verbose = 'TRUE',# Use the "random forest" algorithm
+              trControl = trainControl(method = "cv",number = 10)); # Use 10 folds for cross-validation
+               
+
+trainResults<- predict(trainModel,newData = trainData);
+
+validationData<- predict(pcaData,validation[,-53]);
+validationResults<- predict(trainModel,newData = validationData);
+
+confusionMatrix(training$classe,trainingResults)
+confusionMatrix(validation$classe,validationResults)
 
 
 
+finData<- read.csv(file="C:/Users/Acer User/Documents/coursera/PracticalMachineLearning/pml-testing.csv");
+finData<- finData[,8:siz[2]]
+finData<- finData[,(!naCols & !nsV$nzv)]
 
-pp_hpc <- preProcess(training, method = c("center", "scale"))
+finalPCA<- predict(pcaData,finData[,-53]);
+finalRes<- predict(trainModel,newData = finalPCA)
+#confusionMatrix(finalData$classe,)
 
 
-model <- train(training$classe ~ ., # Survived is a function of the variables we decided to include
-               data = training, # Use the trainSet dataframe as the training data
-               method = "rf",# Use the "random forest" algorithm
-               trControl = trainControl(method = "cv", # Use cross-validation
-                                        number = 10) # Use 5 folds for cross-validation
+               
+               
